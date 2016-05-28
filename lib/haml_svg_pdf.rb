253e6c620af59767d24ea4a24dd0e6ee7fc888e1
Eiji MATSUMOTO <e.mattsan@gmail.com>
2016-05-28 12:38:33 +0900

    impliment haml -> svg -> pdf

--- lib/haml_svg_pdf.rb
+++ lib/haml_svg_pdf.rb
@@ -1,5 +1,24 @@
-require "haml_svg_pdf/version"
+require 'haml_svg_pdf/version'
+require 'haml'
+require 'rsvg2'
+require 'cairo'
 
 module HamlSvgPdf
-  # Your code goes here...
+  def self.dpi=(value)
+    @dpi = value
+  end
+
+  def self.dpi
+    @dpi ||= 72
+  end
+
+  def self.haml_svg_pdf(haml, sink)
+    source = Haml::Engine.new(haml).to_html
+    svg = RSVG::Handle.new_from_data(source)
+    svg.dpi = self.dpi
+    dim = svg.dimensions
+    surface = Cairo::PDFSurface.new('sample.pdf', dim.width, dim.height)
+    cairo = Cairo::Context.new(surface)
+    cairo.render_rsvg_handle(svg)
+  end
 end
