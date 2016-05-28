c80d4216d33aa0e8814a1e8276ed2d790069fa2a
Eiji MATSUMOTO <e.mattsan@gmail.com>
2016-05-28 12:42:13 +0900

    add dpi test

--- spec/haml_svg_pdf_spec.rb
+++ spec/haml_svg_pdf_spec.rb
@@ -4,4 +4,15 @@ describe HamlSvgPdf do
   it 'has a version number' do
     expect(HamlSvgPdf::VERSION).not_to be nil
   end
+
+  context 'dpi' do
+    context 'default' do
+      it { expect(HamlSvgPdf.dpi).to eq 72 }
+    end
+
+    context 'set 90' do
+      before { HamlSvgPdf.dpi = 90 }
+      it { expect(HamlSvgPdf.dpi).to eq 90 }
+    end
+  end
 end
